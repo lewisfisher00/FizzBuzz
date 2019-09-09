34f9506e009128ba5516339f412ea87bfb65416e
FISHER Lewis <lfisher@altranuk.local>
2019-09-09 11:35:10 +0100

    Added Bang

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,14 @@
 for number in range(1, 101):
+    currentString = str(number)
     if (number % 5 == 0) & (number % 3 == 0):
-        print("FizzBuzz")
+        currentString = "FizzBuzz"
     elif number % 5 == 0:
-        print("Buzz")
+        currentString = "Buzz"
     elif number % 3 == 0:
-        print("Fizz")
-    else:
-        print(str(number))
+        currentString = "Fizz"
+    if number % 7 == 0:
+        if str(number) == currentString:
+            currentString = "Bang"
+        else:
+            currentString = currentString + "Bang"
+    print(currentString)
