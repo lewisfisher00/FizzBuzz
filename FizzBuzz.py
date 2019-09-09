6dcb369c56cffea495af80b4bec66e9084907845
FISHER Lewis <lfisher@altranuk.local>
2019-09-09 14:01:58 +0100

    13 working no 17

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,5 +1,5 @@
 maxValue = int(input("Enter maximum value:"))
-for number in range(1, maxValue + 1):
+for number in range(195, maxValue + 1):
     specialList = []
     if (number % 5 == 0) & (number % 3 == 0):
         specialList.append("Fizz")
@@ -16,7 +16,14 @@ for number in range(1, maxValue + 1):
     if number % 11 == 0:
         currentString = "Bong"
     if number % 13 == 0:
-        specialList.insert(0, "Fezz")
+        counter = int(0)
+        while counter < len(specialList):
+            if specialList[counter][0] == "B":
+                indexB = int(counter)
+            counter = counter + 1
+        if counter == len(specialList) + 1:
+            indexB = 0
+        specialList.insert(indexB, "Fezz")
     if (number % 17 == 0) & (specialList != ""):
         specialList.reverse()
     if specialList == "":
