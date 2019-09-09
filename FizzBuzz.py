cef2c6ab3d692b72a7e36836138b7d9190dd11e0
FISHER Lewis <lfisher@altranuk.local>
2019-09-09 11:14:45 +0100

    Task 1

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,9 @@
-print("Start")
+for number in range(1, 101):
+    if (number % 5 == 0) & (number % 3 == 0):
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(str(number))
